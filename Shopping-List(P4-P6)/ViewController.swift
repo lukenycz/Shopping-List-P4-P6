f3ef9fb69f127b4ee0789b1a85a0a3d0eddcca02
Łukasz Nycz <lnycz96@gmail.com>
2021-07-06 10:34:09 +0200

    Initial Commit

---
+++ Shopping-List(P4-P6)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shopping-List(P4-P6)
+//
+//  Created by Łukasz Nycz on 06/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
