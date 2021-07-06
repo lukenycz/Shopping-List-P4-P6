ae5726202f0241d449f9644e59c595cb97e3ed1d
Łukasz Nycz <lnycz96@gmail.com>
2021-07-06 12:14:13 +0200

    Shopping List App - Finished !:D

--- Shopping-List(P4-P6)/ViewController.swift
+++ Shopping-List(P4-P6)/ViewController.swift
@@ -7,13 +7,75 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var shoppingList = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Your Shopping List:"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(refreshList))
+        
+        
+        let addItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItems))
+        let shareIcon = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareList))
+        
+        navigationItem.rightBarButtonItems = [addItem, shareIcon]
     }
 
+    
+    @objc func refreshList() {
+        let ac = UIAlertController(title: "Clear your list?", message: "Remove all items from your list?", preferredStyle: .alert)
+        
+        let clear = UIAlertAction(title: "Remove all", style: .cancel) { (UIAlertAction) in
+            self.shoppingList.removeAll(keepingCapacity: true)
+            self.tableView.reloadData()
+        }
+        ac.addAction(clear)
+        present(ac, animated: true)
+    }
+    
+    @objc func addItems() {
+        let ac = UIAlertController(title: "Enter Your Items", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            [weak self, weak ac] action in
+            guard let item = ac?.textFields?[0].text else { return }
+            self?.submit(item)
+        }
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    @objc func shareList() {
+        let list = shoppingList.joined(separator: "\n")
+        
+        let vc = UIActivityViewController(activityItems: [list], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+    
+    func submit(_ item:String) {
+        let lowerItem = item.lowercased()
+        
+        shoppingList.insert(lowerItem, at: 0)
+        
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        return cell
+    }
+
+    
 
 }
-
